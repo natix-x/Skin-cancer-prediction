0db88a253448d77b5dc08d9e5a650b75932eea31
Natalia Wrześniak <natalia.wrzesniak19@gmail.com>
2024-01-08 12:23:06 +0100

    analysis & grouping done

--- analysis_functions.py
+++ analysis_functions.py
@@ -1,8 +1,8 @@
 import matplotlib.pyplot as plt
-import pandas as pd
+import seaborn as sns
 
 
-def count_plot(df_name, column_name):
+def count_plot(df_name, column_name: str):
     plt.figure(figsize=(8, 4), dpi=200)
     ax = df_name[column_name].value_counts().plot(kind="bar")
     plt.xticks(rotation=0)
@@ -21,3 +21,27 @@ def diagnose(diagnosis):
         return "melanoma"
     else:
         return "other"
+
+
+def group_plot(df_name, column_name: str):
+    fig, ax = plt.subplots(1, 3, figsize=(8, 4))
+
+    sns.countplot(
+        data=df_name[df_name["diagnosis"] == "melanocytic nevi"],
+        x=column_name,
+        ax=ax[0],
+    )
+    sns.countplot(
+        data=df_name[df_name["diagnosis"] == "other"], x=column_name, ax=ax[1]
+    )
+    sns.countplot(
+        data=df_name[df_name["diagnosis"] == "melanoma"], x=column_name, ax=ax[2]
+    )
+
+    ax[0].set_title("melanocytic nevi")
+    ax[1].set_title("other")
+    ax[2].set_title("melanoma")
+
+    fig.suptitle(f"Distribution of {column_name}")
+    fig.tight_layout()
+    plt.show()
