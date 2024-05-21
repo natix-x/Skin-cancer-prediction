d43b9f5d94e01085f7d195f9b51de3feffffed6a
Natalia Wrześniak <natalia.wrzesniak19@gmail.com>
2024-05-21 13:08:05 +0200

    new structure - isolated directory for data analysis added

--- data_analysis/analysis_functions.py
+++ data_analysis/analysis_functions.py
@@ -10,14 +10,13 @@ def count_plot(df_name, column_name: str):
     :return: count plot
     """
     plt.figure(figsize=(8, 4), dpi=200)
-    ax = df_name[column_name].value_counts().plot(kind="bar")
+    ax = df_name[column_name].value_counts().plot(kind="bar", color="#660066")
     plt.xticks(rotation=0)
     for label in ax.containers:
         ax.bar_label(label)
     plt.title(f"Distribution of {column_name}")
     plt.ylabel("count")
     plt.tight_layout()
-    plt.show()
 
 
 def diagnose(diagnosis):
